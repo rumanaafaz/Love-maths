26759acbd5cfe80946d0a1c290a6ae087c27c349
rumanaafaz <rumanaafaz03@gmail.com>
2024-01-10 02:01:56 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`YoŸu clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
-
-    
+    runGame("addition");
 });
 
 /**
@@ -24,11 +23,19 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+
+    }
 
 }
 
@@ -50,7 +57,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
